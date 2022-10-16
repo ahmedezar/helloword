1e9af6755b55f593fcd31396950e8fdcb31758e2
ahmedezar <73957153+ahmedezar@users.noreply.github.com>
2022-10-17 01:01:55 +0200

    first

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+
+	
+	
+	@GetMapping("/helloWorld")
+	public String Hello() {
+		
+		return "hello world";
+	}
+	
+	
+}
